3d915f19cf68d48c99ce6f6feaab6b34113667c8
adititomar123 <adititomar2k8@gmail.com>
2022-06-24 15:26:28 +0530

    Feat: Added interactive download btn style: added footer

--- development/src/pages/home/home.ts
+++ development/src/pages/home/home.ts
@@ -1,3 +1,18 @@
 // to use custom svg, import if from js/ts code, then use it in markup
 // e.g. <svg><use xlink:href="svg/sprite.svg#icon-cancel"></use></svg>
 import '../../svgs/icon-cancel.svg';
+import '../../svgs/icon-linkedin.svg';
+import '../../svgs/icon-github.svg';
+import '../../svgs/icon-gmail.svg';
+
+const downloadLink = document.getElementById('download_CV');
+downloadLink?.addEventListener('click', function () {
+  downloadLink?.classList.add('loader');
+  setTimeout(() => {
+    downloadLink?.classList.add('finished');
+    downloadLink?.classList.remove('loader');
+  }, 3000);
+  setTimeout(() => {
+    downloadLink?.classList.remove('finished');
+  }, 4800);
+});
