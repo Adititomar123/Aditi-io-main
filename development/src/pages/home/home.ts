0a4ba4906c4a670576a1bbd182e8a59d10d4e271
adititomar123 <adititomar2k8@gmail.com>
2022-06-27 20:23:26 +0530

    Feat: Switch theme on clicking a button

--- development/src/pages/home/home.ts
+++ development/src/pages/home/home.ts
@@ -4,6 +4,7 @@ import '../../svgs/icon-cancel.svg';
 import '../../svgs/icon-linkedin.svg';
 import '../../svgs/icon-github.svg';
 import '../../svgs/icon-gmail.svg';
+import '../../svgs/icon-theme.svg';
 
 const downloadLink = document.getElementById('download_CV');
 downloadLink?.addEventListener('click', function () {
@@ -17,6 +18,25 @@ downloadLink?.addEventListener('click', function () {
   }, 4800);
 });
 
+//change theme
+const darkModeToggle = document.querySelector('#theme-toggle');
+darkModeToggle?.addEventListener('click', () => {
+  const setTheme = document.body;
+  setTheme.classList.toggle('darkmode');
+  let theme;
+  if (setTheme.classList.contains('darkmode')) {
+    theme = 'dark';
+  } else {
+    theme = 'light';
+  }
+  localStorage.setItem('pageTheme', JSON.stringify(theme));
+});
+const getTheme = JSON.parse(localStorage.getItem('pageTheme')!);
+if (getTheme === 'dark') {
+  document.body.classList.add('darkmode');
+}
+
+//css lazy loading
 (function () {
   const cssHome = document.createElement('Link') as HTMLAnchorElement;
   cssHome.href = 'css/home.css';
