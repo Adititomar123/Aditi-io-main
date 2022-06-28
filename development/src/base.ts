40e82c7863e0a9b7bb56f3dd6aabd54060196bbb
adititomar123 <adititomar2k8@gmail.com>
2022-06-28 12:32:18 +0530

    Feat: lazy load stylesheet using class

--- development/src/base.ts
+++ development/src/base.ts
@@ -1,7 +1,1 @@
-(function () {
-  const cssBase = document.createElement('Link') as HTMLAnchorElement;
-  cssBase.href = 'css/base.css';
-  cssBase.rel = 'stylesheet';
-  cssBase.type = 'text/css';
-  document.getElementsByTagName('head')[0].appendChild(cssBase);
-})();
+
